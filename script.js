68c6fd2f4cb08e64f5fe3b903f37c7cd17ce3027
Ben Dei <Xelnarash@googlemail.com>
2024-09-06 15:01:28 +0200

    adding JS

---
+++ script.js
@@ -0,0 +1,17 @@
+let questions = [
+{
+        "question" : "wer hat HTML erfunden",
+        "answer_1" : "Weihnachtsmann",
+        "answer_2" : "Chuck Norris",
+        "answer_3" : "Tim Burton",
+        "answer_4" : "Tim Berners-Lee",
+        "right_answer" : 4
+},
+{
+        "question" : "Für was steht HTML",
+        "answer_1" : "Hypertext Markup Language",
+        "answer_2" : "Hyper Trex mit Licht",
+        "answer_3" : "Hyperbeam Tackle MachPunch Leech Life",
+        "answer_4" : "Hypertext Mug Language",
+}
+]
